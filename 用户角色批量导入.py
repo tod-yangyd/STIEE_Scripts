947312d8ee4e671e913505b05aeb0acf5c5858bb
yangyedong <tod.yangyd@foxmail.com>
2023-09-20 17:51:51 +0800

    优化代码

--- 用户角色批量导入.py
+++ 用户角色批量导入.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sshtunnel import SSHTunnelForwarder
 import pymysql
 import datetime
+import json
 
 class Mysql_Connection:
     def __init__(self, db_address, db_user, db_password):
@@ -38,51 +39,73 @@ class Mysql_Connection:
         roleinfo_list = []
 
         for role_name in role_list:
+            sql1 = "select id from member_role where name in ('%s') and is_deleted=0" % role_name
+            result_df = self.querysql(sql1)
             try:
-                sql1= "select id from member_role where name in ('%s') and is_deleted=0" % role_name
-                result_df = self.querysql(sql1)
-                if result_df.at[0, 'id'] > 0:
-                    sql2 = "select a.member_id ,a.organization_id ,b.name,a.role_id,a.role_sp  from \
-                     member_account_role  a left join member_organization b on a.organization_id =b.id \
-                      where role_id in (%d) " % result_df.at[0, 'id']
-                    result_df2 = self.querysql(sql2)
-                    for idx,data in result_df2.iterrows():
-                        org_id_encode = self.encode_number(data['organization_id'])
-                        # role_info = {"member_id": data['member_id'], "org_id": data['organization_id'],
-                        #              "role_id": data['role_id'], "creator": "",
-                        #              "gmt_create": time, "gmt_modified": time,
-                        #              "org_sp": '[{"organizationId":"%s","organizationName":"%s"}]'
-                        #                        % (org_id_encode, data['name']), "role_sp": data['role_sp']}
-                        role_info = {"member_id": data['member_id'], "org_name": data['name'],
-                                     "role_id": data['role_id'], "role_sp": data['role_sp']}
-                        roleinfo_list.append(role_info)
-                else:
-                    print("角色： %s 不存在" % role_name)
+                role_id = result_df.at[0, 'id']
             except Exception as e:
-                print("角色 ： %s 获取角色信息失败，失败原因：%s" % (role_name, e))
+                print("角色 ： %s 在地址：%s 不存在" % (role_name,self.host))
+            else:
+                try:
+                    sql2 = "select c.real_name ,b.name ,a.organization_id  from   member_account_role a  \
+                                          left join member_role b on a.role_id = b.id \
+                                          left join member_account c on a.member_id = c.id\
+                                          where  (a.organization_id <>0 or a.organization_id is not null) \
+                                           and role_id in (%d) " % role_id
+                    result_df2 = self.querysql(sql2)
+                    for idx, data in result_df2.iterrows():
+                        org_id_list = data['organization_id'].split(",")
+                        org_sp_name_list = []
+                        for org_id in org_id_list:
+                            sql3 = "select name from member_organization where id= '%s'" % org_id
+                            result_df = self.querysql(sql3)
+                            org_name = result_df.at[0, 'name']
+                            org_sp_name_list.append(org_name)
+                        role_info = {"member_name": data['real_name'], "org_sp_name_list": org_sp_name_list,
+                                      "role_name": data['name']}
+
+                        roleinfo_list.append(role_info)
+                except Exception as e:
+                    print("角色 ： %s 获取角色信息失败，失败原因：%s,失败行号： %s" % (role_name, e, e.__traceback__.tb_lineno))
+        print ("###########获取角色信息完成####################")
         return roleinfo_list
 
     def trans_org(self,roleinfo_list):
         for role_info in roleinfo_list:
-            org_name_fromdata = role_info['org_name']
+            org_sp_name_list = role_info['org_sp_name_list']
+            role_name = role_info['role_name']
             try:
-                sql = "select id,name from member_organization where is_deleted =0 and name = '%s'" % org_name_fromdata
-                result_df = self.querysql(sql)
-                org_id_encode = self.encode_number(result_df.at[0, 'id'])
-                tmp = {"org_id": result_df.at[0, 'id'], "org_sp": '[{"organizationId":"%s","organizationName":"%s"}]'
-                                                                  % (org_id_encode, org_name_fromdata)}
-                role_info.update(tmp)
+                query_name = "select id,name from member_role  where name='%s' and is_deleted=0" % role_name
+                result_df = self.querysql(query_name)
+                role_id = result_df.at[0, 'id']
             except Exception as e:
-                print("组织名：%s在地址：%s 获取信息失败，失败原因：%s" % (org_name_fromdata,self.host, e))
+                print("角色：%s在地址：%s 获取信息失败，失败原因：%s" % (role_name, self.host, e))
+            else:
+                org_sp = {}
+                for org_sp_name in org_sp_name_list:
+                    sql = "select id,name from member_organization where is_deleted =0 and name = '%s'" % org_sp_name
+                    result_df = self.querysql(sql)
+                    try:
+                        org_id_encode = self.encode_number(result_df.at[0, 'id'])
+                    except Exception as e:
+                        print("组织：%s 在地址：%s 获取信息失败，失败原因：%s" % (org_sp_name, self.host, e))
+                    else:
+                        org_sp_tmp = {"organizationId": org_id_encode, "organizationName": org_sp_name}
+                        org_sp.update(org_sp_tmp)
+                        tmp = {"role_id": role_id, "org_sp": org_sp}
+                        role_info.update(tmp)
         return roleinfo_list
 
     def generate_sql(self,roleinfo_list):
         sql_list = []
+        time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         for role_info in roleinfo_list:
-            sql = "insert into member_account_role (member_id, organization_id ,role_id ,creator ,gmt_create \
-            ,gmt_modified ,organization_sp ,role_sp ,position_id) values()" % (role_info['member_id'],)
-
-
+            sql = ("insert into member_account_role (member_id, organization_id ,role_id ,creator ,gmt_create \
+            ,gmt_modified ,organization_sp ,role_sp ,position_id) values({0},{1},{2},{3},{4},{5},{6},{7},{8})".format
+                   (role_info['member_id'], role_info['org_id'],role_info['role_id'], "", time, time,
+                    role_info['org_sp'], role_info['role_sp'], ""))
+            sql_list.append(sql)
+        return sql_list
 
 class Mysql_Connection_ssh(Mysql_Connection):
     def __init__(self, db_address, db_user, db_password, ssh_private_key_password):
@@ -125,7 +148,8 @@ if __name__ == '__main__':
     if Sync_from_pi:
         roleinfo_list = pro_conn.get_role_info(role_list)
         roleinfo_list1 = test_conn.trans_org(roleinfo_list)
-        print(roleinfo_list1)
+        sql_list = pro_conn.generate_sql(roleinfo_list1)
+        print(sql_list)
     else:
         roleinfo_list = test_conn.get_role_info(role_list)
         roleinfo_list1 = pro_conn.trans_org(roleinfo_list)
